23362220a492cc076542187295eb14872ae32010
hedaukartik <hedaukartik2009@gmail.com>
2025-01-23 23:22:21 +0530

    fixing react code to test e2e

--- src/index.tsx
+++ src/index.tsx
@@ -2,33 +2,32 @@ import { lazy, Suspense } from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./components/errorPage";
-import "./index.scss";
 
 const Home = lazy(() => import("./layouts/home"));
 const Contacts = lazy(() => import("./layouts/contacts"));
 const Contact = lazy(() => import("./layouts/contacts/id"));
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/contacts",
-    element: <Contacts />,
-  },
-  {
-    path: "/contacts/:id",
-    element: <Contact />,
-  },
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "/contacts",
+		element: <Contacts />,
+	},
+	{
+		path: "/contacts/:id",
+		element: <Contact />,
+	},
 ]);
 
 const rootDiv = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootDiv);
 
 root.render(
-  <Suspense fallback={<>Loading...</>}>
-    <RouterProvider router={router} />
-  </Suspense>
+	<Suspense fallback={<>Loading...</>}>
+		<RouterProvider router={router} />
+	</Suspense>
 );
--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
-import "./index.scss";
 
 function Home() {
-  return (
-    <div>
-      <h1 className="title">
-        Welcome to <span>Bun React CSR App</span>
-      </h1>
-      <Link to={`contacts`}>View contacts</Link>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="title">
+				Welcome to <span>Bun React CSR App</span>
+			</h1>
+			<Link to={`contacts`}>View contacts</Link>
+		</div>
+	);
 }
 
 export default Home;
